5e46d8897058e035ef77715b8362543959b68b93
chrisggdev <81429553+chrisggdev@users.noreply.github.com>
2022-02-20 13:58:51 -0500

    wip

--- components/DividerImg/index.js
+++ components/DividerImg/index.js
@@ -18,7 +18,7 @@ export default function DividerImg(props) {
         className="h-[90vh] relative"
         style={{ backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.2) 0%, rgba(0, 0, 0, 0.2) 55.28%), url(${image})`, backgroundSize: "cover", backgroundPosition: `${props.position}`}}
       >
-          <p className='caption'>caption</p>
+          <p className='caption'></p>
           </section>
     )
 }
