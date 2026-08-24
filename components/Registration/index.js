de2766d7e26b3e4589187e604ce9b2d9f3ccd823
ChrisGeorgeDev <chris.george@greatgulf.com>
2026-08-24 13:19:12 -0400

    bump

--- components/Registration/index.js
+++ components/Registration/index.js
@@ -30,6 +30,7 @@ export default function Registration() {
    <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
 </svg>
               289.498.3213</p>
+              
           
             <p className="text-gvhGold-100 flex items-center md:text-lg">
             <svg xmlns="http://www.w3.org/2000/svg" className="icon mr-2 h-6 w-6 icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="#192128" fill="white" stroke-linecap="round" stroke-linejoin="round">
